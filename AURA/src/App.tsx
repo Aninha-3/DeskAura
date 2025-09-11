f8b8b4aeab97748c0c19b4d84107cab7ba96f21a
bru775 <brunesco2008@gmail.com>
2025-09-11 11:05:28 -0300

    Atualizando as paginas de cadastro e login

--- AURA/src/App.tsx
+++ AURA/src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Home from './pages/Home';
+import { Footer }  from './components/Footer';
+// If your file is named 'Login.tsx' or 'Login.jsx', use the correct extension:
 import Cadastro from './pages/Cadastro/Cadastro';
-import { Footer } from './components/Footer';
+// If the file is actually named 'login.tsx', change the import to:
+ // import Login from "./pages/login";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/" element={<Home />} />
           <Route path='/Cadastro' element={<Cadastro />} />
           <Route path="/Footer" element={<Footer />} />
--- AURA/src/pages/Cadastro/Cadastro.tsx
+++ AURA/src/pages/Cadastro/Cadastro.tsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
 import styles from './Cadastro.module.css';
 import { Footer } from '../../components/Footer';
+import { useNavigate } from 'react-router-dom';
+
 
 const Cadastro = () => {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [confirmacao, setConfirmacao] = useState('');
+  const navigate = useNavigate();
 
+
+
+  // Aplica a classe no <body> quando o componente monta
   useEffect(() => {
     document.body.className = styles.page;
     return () => {
@@ -32,6 +39,10 @@ const Cadastro = () => {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
+        <button onClick={() => navigate('/')} className={styles.botaoVoltar}>
+          <FaArrowLeft size={20} />
+        </button>
+
         <h2 className={styles.title}>Cadastro</h2>
         <form onSubmit={handleSubmit} className={styles.form}>
           <div className={styles.field}>
@@ -85,7 +96,7 @@ const Cadastro = () => {
           <button type="submit" className={styles.button}>Cadastrar</button>
 
           <p className={styles.loginText}>
-            Já possui conta? <a href="/login" className={styles.loginLink}>Logar</a>
+            Já possui conta? <a href="/Login" className={styles.loginLink}>Logar</a>
           </p>
         </form>
       </div>
