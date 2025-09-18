169a95cda8a6c4a658fcbb0d943c5b83d476402b
Bruno Senna <brunesco2008@gmail.com>
2025-09-18 14:45:01 -0300

    INICIANDO BACK
    organizando ambiente

--- Backend/Middleware/corsConfig.js
+++
@@ -1,9 +0,0 @@
-const cors = require('cors');
-
-const corsOptions = {
-  origin: 'http://localhost:5173', // seu frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-};
-
-module.exports = cors(corsOptions);
