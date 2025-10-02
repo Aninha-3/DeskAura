38c108812d53eb4cdc152441450e328ed2b40381
PedrosoEnzo <enzopedroso77@gmail.com>
2025-10-02 09:58:56 -0300

    Criando função para quando o formulário for enviado na pagina de login

--- AURA/src/pages/Login/Login.tsx
+++ AURA/src/pages/Login/Login.tsx
@@ -7,12 +7,20 @@ function Login () {
 const [email, setEmail] = useState('')
 const [senha, setSenha] = useState('')
 
+
+//Essa função lida com a mudança no campo de email
 const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
 };
 
+//Fazendo o mesmo para a senha
 const handleSenhaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 setSenha(event.target.value)
+}
+
+//Essa função vai ser usada pra quando o formulário for enviado
+const handleLoginSubmit = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
 }
 }
 
