e940114a94e18bcda0fefc135c10c0a1292e45ee
PedrosoEnzo <enzopedroso77@gmail.com>
2025-10-02 14:18:17 -0300

    Dando entrada nos dados de Nome Email e Pass, na pagina de cadastro

--- AURA/src/pages/Cadastro/Cadastro.tsx
+++ AURA/src/pages/Cadastro/Cadastro.tsx
@@ -20,8 +20,33 @@ function Cadastro() {
       console.error(error);
     }
   };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={nome}
+        placeholder="Nome"
+        onChange={(e) => setNome(e.target.value)}
+        required
+      ></input>
+      <input type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+       <input
+      type="password"
+      placeholder="Senha"
+      value={senha}
+      onChange={(e) => setSenha(e.target.value)}
+      required
+    />
+    <button type="submit">Cadastrar</button>
+    {mensagem && <p>{mensagem}</p>}
+    </form>
+  )
 
- 
 }
 
 export default Cadastro;
