4a7c66828e58481c8ac556756e672c93cdf67bbc
PedrosoEnzo <enzopedroso77@gmail.com>
2025-10-02 10:50:41 -0300

    Escalando prioridade para footer, na pagina app.tsx

--- AURA/src/App.tsx
+++ AURA/src/App.tsx
@@ -1,36 +1,38 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Cadastro from './pages/Cadastro/Cadastro';
+import Login from './pages/Login/Login';
 import Simulador from './pages/Simulador/Simulador';
 import Chatbot from './components/Assistente'; 
 import { Footer } from './components/Footer';
 
-
-function App() {
-  
+// Layout com lógica do footer
+function Layout() {
   const location = useLocation();
 
-  // Definir rotas que NÃO devem ter footer
+  // Rotas que não terão footer
   const noFooterRoutes = ['/cadastro', '/login'];
+  const showFooter = !noFooterRoutes.includes(location.pathname.toLowerCase());
 
-  const showFooter = !noFooterRoutes.includes(location.pathname);
-  
   return (
-    <Router>
-      <div className="App">
-        {/* Routes principais */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/simulador" element={<Simulador />} />
-        </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/simulador" element={<Simulador />} />
+      </Routes>
 
-        {/* Sempre visível em todas as páginas */}
-        <Chatbot />
-           {showFooter && <Footer />}
-      </div>
-    </Router>
+      <Chatbot />
+      {showFooter && <Footer />}
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
+  );
+}
