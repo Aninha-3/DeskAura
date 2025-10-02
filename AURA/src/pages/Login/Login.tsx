69476f63a058faa54b9125ffa386d9a41c08ac32
PedrosoEnzo <enzopedroso77@gmail.com>
2025-10-02 09:06:01 -0300

    Criando campos de entrada para email e senha

--- AURA/src/pages/Login/Login.tsx
+++ AURA/src/pages/Login/Login.tsx
@@ -1,2 +1,9 @@
 import React, {useState,useEffect} from "react";
 import styles from './Login.module.css'
+
+function Login () {
+
+const [email, sertEmail] = useState('')
+const [senha, setSenha] = useState('')
+}
+
