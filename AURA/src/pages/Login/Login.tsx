cf31d250bc75d104b5985fb4b58d13eeecfb9612
PedrosoEnzo <enzopedroso77@gmail.com>
2025-10-02 09:49:23 -0300

    Criando variaveis de estado na pagina de login

--- AURA/src/pages/Login/Login.tsx
+++ AURA/src/pages/Login/Login.tsx
@@ -3,7 +3,16 @@ import styles from './Login.module.css'
 
 function Login () {
 
-const [email, sertEmail] = useState('')
+    //Criando variaveis de estado
+const [email, setEmail] = useState('')
 const [senha, setSenha] = useState('')
+
+const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+};
+
+const handleSenhaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+setSenha(event.target.value)
+}
 }
 
